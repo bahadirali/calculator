87b99e38936a268c65f9b371d0508eeabf703419
bahadirali <bahadirali25@gmail.com>
2019-06-30 17:39:43 -0500

    Do keyboard and screen text

--- src/App.js
+++ src/App.js
@@ -6,44 +6,82 @@ export default class App extends React.Component{
     super(props);
 
     this.state = {
-      calcText: '',
+      screenText: '',
     };
 
     this.handleButtonInput = this.handleButtonInput.bind(this);
+    this.clearScreen = this.clearScreen.bind(this);
   }
 
 
   handleButtonInput(event, input){
-    const currentCalcText = this.state.calcText;
+    const currentScreenText = this.state.screenText;
     this.setState({
-      calcText:  currentCalcText + input,
+      screenText:  currentScreenText + input,
     });
   }
 
+  clearScreen(event){
+    this.setState({
+      screenText: '',
+    });
+
+  }
+
   render(){
-    let numbers = [];
-    for(let i=2; i>=0; i--){
+
+    let keyboard = [];
+    let rows = [];
+    const buttonSymbols = [
+                            ['7','8','9','-'],
+                            ['4','5','6','+'],
+                            ['1','2','3','x'],
+                            ['0','c','=','/'],
+                          ];
+
+    rows.push(
+      <tr>
+        <td colSpan="4">
+          <button onClick={this.clearScreen}>
+            clear
+          </button>
+        </td>
+      </tr>
+    );
+
+    for(let i=0; i<4; i++){
       let cells = [];
-      for(let j=1; j<=3; j++){
+      for(let j=0; j<4; j++){
         cells.push(<td>
-          <button onClick={(e) => {this.handleButtonInput(e, i*3 + j)}}>{i*3 + j}</button>
+          <button onClick={(e) => {this.handleButtonInput(e, buttonSymbols[i][j])}}>
+            {buttonSymbols[i][j]}
+          </button>
         </td>);
       }
-      numbers.push(
+      
+      rows.push(
         <tr>
           {cells}
         </tr>
       );
     }
+
+    /*
+    keyboard.push(
+        {rows}
+    );
+    */
+   keyboard = rows;
+
     return (
       <div className="App">
         <table>
           <tr>
-            <td>
-              {this.state.calcText}
+            <td colSpan="4">
+              {this.state.screenText}
             </td>
           </tr>
-          {numbers}
+          {keyboard}
         </table>
       </div>
     );
